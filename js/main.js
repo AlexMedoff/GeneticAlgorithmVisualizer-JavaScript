9e604b9048c8b1671bbaf090e80e539ffb02cb37
AlexMedoff <alexfmedoff@gmail.com>
2018-12-14 19:10:07 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var yaleLogo;
+var theLogo;
 var popSize = 10;
 var thePopulation;
 var numOfGenerations = 0;
@@ -18,14 +18,14 @@ var note;
 var youtubelink;
 
 function preload() {
-	yaleLogo = loadImage("https://i.imgur.com/IkeiqVx.jpg");
-	note_string = "Please Note: This program requires that the user be using a computer with a strong processor, usually found in desktop computers. If you notice that the box to the left is not producing the Yale Logo image but instead appears as a jumble of blue and white circles, please visit the Youtube link below to see what it should look like."
-	paragraph_string = "Hello! Welcome to my website dedicated to a program I wrote for Yale. On the left of your screen, you may notice a small box being populated with a bunch of blue and white dots. Slowly, those circles will align themselves so that they spell out 'Yale', and they will figure out how to do this completely on their own. This is what is known as a Hill Climbing Genetic Algorithm. What that means is that the program simulates evolution. At first, a population of 10 images are assembled, each with a few dots on a white background. The program determines which of those 10 images looks the most like the Yale logo, and it deems that image the 'healthiest gene'. The population then evolves, meaning that each image in the population is replaced by a copy of that healthiest gene, and then each one adds a few more dots. This process keeps repeating itself, and eventually, the image resembles the Yale logo. Each time the population evolves, we call that a new generation (that number below the box represents the number of generations so far). Each time you refresh this page, you will notice that the program takes a different path to reach the same destination. Below is a link to the code I wrote to develop this program."
+	theLogo = loadImage("https://i.imgur.com/IkeiqVx.jpg");
+	note_string = "Please Note: This program requires that the user be using a computer with a strong processor, usually found in desktop computers. If you notice that the box to the left is not producing the logo image but instead appears as a jumble of blue and white circles, please visit the Youtube link below to see what it should look like."
+	paragraph_string = "Hello! Welcome to my website dedicated to a program I wrote. On the left of your screen, you may notice a small box being populated with a bunch of blue and white dots. Slowly, those circles will align themselves so that they spell out the school's logo, and they will figure out how to do this completely on their own. This is what is known as a Hill Climbing Genetic Algorithm. What that means is that the program simulates evolution. At first, a population of 10 images are assembled, each with a few dots on a white background. The program determines which of those 10 images looks the most like the logo, and it deems that image the 'healthiest gene'. The population then evolves, meaning that each image in the population is replaced by a copy of that healthiest gene, and then each one adds a few more dots. This process keeps repeating itself, and eventually, the image resembles the logo. Each time the population evolves, we call that a new generation (that number below the box represents the number of generations so far). Each time you refresh this page, you will notice that the program takes a different path to reach the same destination. Below is a link to the code I wrote to develop this program."
 }
 
 function setup() {
 	pixelDensity(1);
-	cvs = createCanvas(yaleLogo.width, yaleLogo.height);
+	cvs = createCanvas(theLogo.width, theLogo.height);
 	generationsText = createElement('p', 'Generations = ' + numOfGenerations);
 	generationsText.id("generations");
 	header = createElement('h1', "Alex Medoff's Genetic Algorithm");
@@ -36,7 +36,7 @@ function setup() {
 	link = createElement('a', "Link to Code");
 	link.id("viewCode");
 	var a = document.getElementById("viewCode");
-	a.href = "https://github.com/AlexMedoff/HillClimbGeneticAlgorithmYaleLogo";
+	a.href = "https://github.com/AlexMedoff/HillClimbGeneticAlgorithm";
 	youtubeLink = createElement('a', 'Click Here to See Youtube Video');
 	youtubeLink.id("ytlink");
 	var a2 = document.getElementById("ytlink");
@@ -85,7 +85,7 @@ function setup() {
 	youtubeLink.style("margin-top", "10px");
 	youtubeLink.style("order", "5");
 
-	thePopulation = new Population(yaleLogo, popSize);
+	thePopulation = new Population(theLogo, popSize);
 }
 
 function draw() {
